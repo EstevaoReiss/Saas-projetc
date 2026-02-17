a62ca9c2a9f5bb48b96ad26df241421e12f65e54
estevaocastro <129298689+EstevaoReiss@users.noreply.github.com>
2026-02-16 22:07:46 -0300

    fix: correção da tela

--- vite-project/src/pages/taskPage.jsx
+++ vite-project/src/pages/taskPage.jsx
@@ -17,7 +17,7 @@ function TaskPage() {
   return (
 
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
 
           <button onClick={BackPage}>
@@ -26,7 +26,7 @@ function TaskPage() {
        
                 <h1 className="text-slate-100 text-3xl font-bold text-center  ">Detalhes da Tarefa</h1>
                  </div>
-                 
+
         <div className="bg-slate-200 p-4 rounded-md">
       <h1 className="text-slate-600 font-bold text-xl">{title || "No title"}</h1>
       <p className="text-slate-600">{description || "No description"}</p>
